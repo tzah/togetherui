70e35f3510ead8f2dd04fe6e9b7111a8dafdde5c
tzahinakash <tzahinakash2000@gmail.com>
2020-06-03 16:07:17 +0300

    create the layout and starting the profile picture component

---
+++ src/components/mainPage/ProfileImage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="profileImage">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ProfileImage"
+    }
+</script>
+
+<style scoped lang="scss">
+    .profileImage {
+        background-color: dimgrey;
+        border-radius: 70px;
+        width: 100px;
+        height: 90px;
+    }
+</style>
---
+++ src/components/mainPage/shell.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-container fluid fill-height>
+        <v-row no-gutters>
+            <v-col cols="3" class="navBar">
+                <slot name="navBar"/>
+            </v-col>
+            <v-col cols="9" class="content">
+                <slot name="content"/>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "shell"
+    }
+</script>
+
+<style scoped lang="scss">
+    .navBar{
+        height: 100vh;
+    }
+    .content{
+        height: 100vh;
+    }
+    .container{
+        padding: unset;
+    }
+</style>
